d55030113c93020173efdb9c5fbfac7ebda722e6
DucLong1206 <longhakdcn.co@gmail.com>
2026-02-24 17:35:47 +0700

    feat: tạo model EF Core cho các bảng trung tâm gia sư

---
+++ Models/Entities/CoreEntities.cs
@@ -0,0 +1,104 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace He_thong_Quan_Ly_trung_tam_gia_su.Models.Entities;
+
+[Table("DM_tinh")]
+public class DmTinh
+{
+    [Key]
+    public int ID { get; set; }
+
+    [Required, MaxLength(500)]
+    public string Name { get; set; } = string.Empty;
+
+    public ICollection<DmXa> DmXas { get; set; } = new List<DmXa>();
+}
+
+[Table("DM_XA")]
+public class DmXa
+{
+    [Key]
+    public int ID { get; set; }
+
+    [Required, MaxLength(500)]
+    public string Name { get; set; } = string.Empty;
+
+    [Column("IDTInh")]
+    public int IDTinh { get; set; }
+
+    [ForeignKey(nameof(IDTinh))]
+    public DmTinh? DmTinh { get; set; }
+}
+
+[Table("MonHoc")]
+public class MonHoc
+{
+    [Key]
+    public int ID { get; set; }
+
+    [Required, MaxLength(500)]
+    public string Name { get; set; } = string.Empty;
+}
+
+[Table("TaiKhoan")]
+public class TaiKhoan
+{
+    [Key]
+    public int ID { get; set; }
+
+    [Required]
+    public string PassWord { get; set; } = string.Empty;
+
+    public bool TypeUsser { get; set; }
+
+    public int IDuser { get; set; }
+
+    [Required]
+    public string Name { get; set; } = string.Empty;
+
+    public bool IsAction { get; set; }
+}
+
+[Table("NhanVien")]
+public class NhanVien
+{
+    [Key]
+    public int ID { get; set; }
+
+    public int IDTK { get; set; }
+
+    [Required, MaxLength(500)]
+    public string Name { get; set; } = string.Empty;
+
+    public string DiaChi { get; set; } = string.Empty;
+
+    [MaxLength(12)]
+    public string SDT { get; set; } = string.Empty;
+
+    public int IDXa { get; set; }
+}
+
+[Table("USER")]
+public class User
+{
+    [Key]
+    public int ID { get; set; }
+
+    public int IDTK { get; set; }
+
+    [Required, MaxLength(500)]
+    public string Name { get; set; } = string.Empty;
+
+    public string DiaChi { get; set; } = string.Empty;
+
+    [MaxLength(12)]
+    public string SDT { get; set; } = string.Empty;
+
+    public int IDXa { get; set; }
+
+    [MaxLength(50)]
+    public string STK { get; set; } = string.Empty;
+
+    public int NganHang { get; set; }
+}
---
+++ Models/Entities/LopHocEntities.cs
@@ -0,0 +1,80 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace He_thong_Quan_Ly_trung_tam_gia_su.Models.Entities;
+
+[Table("LopHoc")]
+public class LopHoc
+{
+    [Key]
+    public int ID { get; set; }
+
+    public int idnguoitao { get; set; }
+
+    public int idnguoinhan { get; set; }
+
+    public int sobuoi { get; set; }
+
+    [Column(TypeName = "decimal(18,0)")]
+    public decimal sotienMotBuoi { get; set; }
+
+    public DateTime ngaytao { get; set; }
+
+    public int trangthai { get; set; }
+
+    public int isdetele { get; set; }
+
+    public string DIaChi { get; set; } = string.Empty;
+
+    public int IDxa { get; set; }
+
+    public double PhiMoiGioi { get; set; }
+}
+
+[Table("LopHoc_BuoiHoc")]
+public class LopHocBuoiHoc
+{
+    [Key]
+    public int ID { get; set; }
+
+    public int IDLop { get; set; }
+
+    public int BuoiSo { get; set; }
+
+    public DateTime NgayHoc { get; set; }
+
+    public TimeSpan GioBatDau { get; set; }
+
+    public TimeSpan GioKetThuc { get; set; }
+
+    public int TrangThai { get; set; }
+
+    public string Lydo { get; set; } = string.Empty;
+}
+
+[Table("LopHoc_LichSu")]
+public class LopHocLichSu
+{
+    [Key]
+    public int ID { get; set; }
+
+    public int sobuoi { get; set; }
+
+    [Column(TypeName = "decimal(18,0)")]
+    public decimal sotienMotBuoi { get; set; }
+
+    public DateTime ngayThayDoi { get; set; }
+
+    public int trangthai { get; set; }
+
+    public string DIaChi { get; set; } = string.Empty;
+
+    public int IDxa { get; set; }
+
+    [Column(TypeName = "decimal(18,0)")]
+    public decimal PhiMoiGioi { get; set; }
+
+    public int IDNGuoiThayDoi { get; set; }
+
+    public int IDLop { get; set; }
+}
