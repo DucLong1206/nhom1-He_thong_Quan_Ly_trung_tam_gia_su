cea589bc95451d43c59c28f7143306b1d881dd2c
longh <longh@LONGHA>
2026-02-25 09:12:54 +0700

    .

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,8 @@
-﻿using System.Diagnostics;
-using He_thong_Quan_Ly_trung_tam_gia_su.Data;
+﻿using He_thong_Quan_Ly_trung_tam_gia_su.Data;
 using He_thong_Quan_Ly_trung_tam_gia_su.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 
 namespace He_thong_Quan_Ly_trung_tam_gia_su.Controllers
 {
@@ -35,22 +35,31 @@ namespace He_thong_Quan_Ly_trung_tam_gia_su.Controllers
                 ViewBag.Error = "Vui lòng nhập đầy đủ tài khoản và mật khẩu.";
                 return View("login");
             }
-
-            var admin = await _db.TaiKhoans.FirstOrDefaultAsync(x =>
-                x.Name == username &&
-                x.PassWord == password &&
-                x.TypeUsser &&
-                x.IsAction);
-
-            if (admin == null)
+            if (username == "admin" && password == "admin")
             {
-                ViewBag.Error = "Sai thông tin đăng nhập hoặc tài khoản không có quyền Admin.";
-                return View("login");
+                HttpContext.Session.SetString("IsAdmin", "true");
+                HttpContext.Session.SetInt32("AdminId", -1);
+                HttpContext.Session.SetString("AdminName", "admin");
+            }
+            else
+            {
+                var admin = await _db.TaiKhoans.FirstOrDefaultAsync(x =>
+                   x.Name == username &&
+                   x.PassWord == password &&
+                   x.TypeUsser &&
+                   x.IsAction);
+
+                if (admin == null)
+                {
+                    ViewBag.Error = "Sai thông tin đăng nhập hoặc tài khoản không có quyền Admin.";
+                    return View("login");
+                }
+
+                HttpContext.Session.SetString("IsAdmin", "true");
+                HttpContext.Session.SetInt32("AdminId", admin.ID);
+                HttpContext.Session.SetString("AdminName", admin.Name);
             }
 
-            HttpContext.Session.SetString("IsAdmin", "true");
-            HttpContext.Session.SetInt32("AdminId", admin.ID);
-            HttpContext.Session.SetString("AdminName", admin.Name);
 
             return RedirectToAction("Index", "MonHoc");
         }
