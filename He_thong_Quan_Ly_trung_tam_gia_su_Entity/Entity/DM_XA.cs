093ab46cf16a9db6380aed694b9c66fac18ed832
longh <longh@LONGHA>
2026-03-11 02:18:46 +0700

    .

--- He_thong_Quan_Ly_trung_tam_gia_su_Entity/Entity/DM_XA.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace He_thong_Quan_Ly_trung_tam_gia_su_Entity.Entity
-{
-    public class DM_XA
-    {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public int IDTInh { get; set; }
-    }
-}
