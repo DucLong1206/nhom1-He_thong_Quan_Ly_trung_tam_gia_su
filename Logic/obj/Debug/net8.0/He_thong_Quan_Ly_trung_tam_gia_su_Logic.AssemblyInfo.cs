551df44decfcb62f6782b96af7933570af069181
longh <longh@LONGHA>
2026-03-13 14:55:23 +0700

    .

--- Logic/obj/Debug/net8.0/He_thong_Quan_Ly_trung_tam_gia_su_Logic.AssemblyInfo.cs
+++ Logic/obj/Debug/net8.0/He_thong_Quan_Ly_trung_tam_gia_su_Logic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("He_thong_Quan_Ly_trung_tam_gia_su_Logic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e3aadab15b849641d2a27a984771900a07967bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb72b8db0f99bf25878115504ca4a3739ab9e47f")]
 [assembly: System.Reflection.AssemblyProductAttribute("He_thong_Quan_Ly_trung_tam_gia_su_Logic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("He_thong_Quan_Ly_trung_tam_gia_su_Logic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
