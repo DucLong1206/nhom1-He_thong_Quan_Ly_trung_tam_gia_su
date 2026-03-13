6b4e5eaf8448d7b37c9b393b9d63a0b52a06eca5
DucLong1206 <longhakdcn.co@gmail.com>
2026-03-13 16:51:59 +0700

    Add tutor email alerts for overdue lesson start/end in LopHoc view

--- web/Controllers/MonHocController.cs
+++ web/Controllers/MonHocController.cs
@@ -1,4 +1,5 @@
-using He_thong_Quan_Ly_trung_tam_gia_su_Entity.Entity;
+﻿using He_thong_Quan_Ly_trung_tam_gia_su_Entity.Entity;
+using He_thong_Quan_Ly_trung_tam_gia_su.Infrastructure.Security;
 using He_thong_Quan_Ly_trung_tam_gia_su_Logic.ILogic;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,14 +18,18 @@ public class MonHocController : Controller
 
     public IActionResult Index()
     {
-        if (HttpContext.Session.GetString("UserId") == null)
-        {
-            return RedirectToAction("login", "Home");
-        }
+        var guardResult = SessionAccessGuard.EnsureUserType(this, 2);
+        if (guardResult != null)
+            return guardResult;
+
         return View();
     }
     public JsonResult getlist(string? keyword, int? monHocId, int? xaId, string sort = "name_asc")
     {
+        var guardResult = SessionAccessGuard.EnsureUserType(this, 2);
+        if (guardResult != null)
+            return Json(new { success = false, message = "Bạn không có quyền truy cập vào khu vực này." });
+
         var vm = _mh.GetListGiaSu();
 
         return Json(new { data = vm });
@@ -32,6 +37,10 @@ public class MonHocController : Controller
 
     public IActionResult TutorDetail(string? tutorName, string? mon, string? xa, decimal? giaTheoGio, int? ID, int? IDXa, int? IDMon, int? Trinhdo, string? avata)
     {
+        var guardResult = SessionAccessGuard.EnsureUserType(this, 2);
+        if (guardResult != null)
+            return guardResult;
+
         ViewData["TutorName"] = tutorName;
         ViewData["Mon"] = mon;
         ViewData["Xa"] = xa;
@@ -48,6 +57,10 @@ public class MonHocController : Controller
     [HttpPost]
     public JsonResult Save([FromBody] SaveLop model)
     {
+        var guardResult = SessionAccessGuard.EnsureUserType(this, 2);
+        if (guardResult != null)
+            return Json(new { success = false, message = "Bạn không có quyền truy cập vào khu vực này." });
+
         string mess = "";
         model.lop.ngaytao = DateTime.Now;
         model.lop.idnguoitao = HttpContext.Session.GetInt32("UserId");
---
+++ web/Infrastructure/Security/SessionAccessGuard.cs
@@ -0,0 +1,51 @@
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace He_thong_Quan_Ly_trung_tam_gia_su.Infrastructure.Security;
+
+public static class SessionAccessGuard
+{
+    public static IActionResult? EnsureLoggedIn(Controller controller)
+    {
+        if (controller.HttpContext.Session.GetInt32("UserId") != null)
+        {
+            return null;
+        }
+
+        return controller.RedirectToAction("login", "Home");
+    }
+
+    public static IActionResult? EnsureUserType(Controller controller, int requiredType)
+    {
+        return EnsureUserTypes(controller, requiredType);
+    }
+
+    public static IActionResult? EnsureUserTypes(Controller controller, params int[] allowedTypes)
+    {
+        var loginResult = EnsureLoggedIn(controller);
+        if (loginResult != null)
+        {
+            return loginResult;
+        }
+
+        var typeUsser = controller.HttpContext.Session.GetInt32("TypeUsser");
+        if (typeUsser.HasValue && allowedTypes.Contains(typeUsser.Value))
+        {
+            return null;
+        }
+
+        controller.TempData["AccessDeniedMessage"] = "Bạn không có quyền truy cập vào khu vực này.";
+
+        if (typeUsser == 1)
+        {
+            return controller.RedirectToAction("TutorDashboard", "Home");
+        }
+
+        if (typeUsser == 2)
+        {
+            return controller.RedirectToAction("Index", "MonHoc");
+        }
+
+        return controller.RedirectToAction("Index", "Home");
+    }
+}
