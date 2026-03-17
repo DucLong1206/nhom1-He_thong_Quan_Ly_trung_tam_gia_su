13d20d49fa84fb254bef5c647fa044f7122fcc46
longh <longh@LONGHA>
2026-03-17 13:35:23 +0700

    .

--- He_thong_Quan_Ly_trung_tam_gia_su_Entity/obj/Debug/net8.0/He_thong_Quan_Ly_trung_tam_gia_su_Entity.AssemblyInfo.cs
+++ He_thong_Quan_Ly_trung_tam_gia_su_Entity/obj/Debug/net8.0/He_thong_Quan_Ly_trung_tam_gia_su_Entity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("He_thong_Quan_Ly_trung_tam_gia_su_Entity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b2aefbf1461c2855bf7353b0cca121ec3fa0b22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8be0bd7c0a95489b27f9bfd1fea0bd057cc800ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("He_thong_Quan_Ly_trung_tam_gia_su_Entity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("He_thong_Quan_Ly_trung_tam_gia_su_Entity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
